456b738e84635bdd79d3daf4f8af22174c40b069
francinelucca <francinelucca@hotmail.com>
2020-02-15 15:27:24 -0400

    fix some css issues

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, catchError, tap } from 'rxjs/operators';
 
 import { Pokemon } from './pokemon';
 
@@ -17,14 +17,18 @@ export class PokemonService {
   getPokemons(offset: number = 0, limit: number = 20): Observable<any>{
     const url = `${this.pokemonsApiBaseUrl}?offset=${offset}&limit=${limit}`;
     return this.http.get(url).pipe(
-      map(this.extractData)
+      map(this.extractData),
+      tap(_ => this.log('fetched pokemons')),
+      catchError(this.handleError<Pokemon[]>('get pokemons',[]))
     )
   }
 
   getPokemon(id: number): Observable<any>{
     const url = `${this.pokemonsApiBaseUrl}/${id}`;
     return this.http.get(url).pipe(
-      map(this.extractData)
+      map(this.extractData),
+      tap(_ => this.log(`fetched pokemon is=${id}`)),
+      catchError(this.handleError<Pokemon>(`getPokemon id=${id}`))
     );
   }
 
@@ -32,4 +36,19 @@ export class PokemonService {
     let body = res;
     return body || { };
   }
+
+  private log(message: string) {
+    console.log(message);
+  }
+
+  private handleError<T> (operation = 'operation', result?: T){
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed:${error.message}`);
+
+      return of(result as T);
+    }
+  }
+
 }
